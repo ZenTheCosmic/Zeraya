d578645593b3096970e3d63ac063f4370afd8a1a
ZenTheCosmic <valdez12john@gmail.com>
2026-04-28 21:37:21 +0800

    Update guildBanAdd.js

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -1,1 +1,32 @@
 
+import { Events, EmbedBuilder } from 'discord.js';
+
+export default {
+  name: Events.GuildBanAdd,
+  once: false,
+
+  async execute(ban) {
+
+    const channel = ban.guild.channels.cache.get("1489567134521753761");
+    if (!channel) return console.log("❌ Ban channel not found");
+
+    try {
+      const embed = new EmbedBuilder()
+        .setColor("#FF0000")
+        .setTitle("🚫 User Banned")
+        .setDescription(
+`<@${ban.user.id}> has been permanently removed from the server.`
+        )
+        .setThumbnail(ban.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setFooter({
+          text: `Member #${ban.guild.memberCount}`
+        })
+        .setTimestamp();
+
+      await channel.send({ embeds: [embed] });
+
+    } catch (err) {
+      console.error("❌ Ban Error:", err);
+    }
+  }
+};
